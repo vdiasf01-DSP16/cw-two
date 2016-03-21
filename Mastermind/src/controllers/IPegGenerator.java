c44667c8d7d96a1cfe421de68002a0364a7ae8ee
Pedro Gordo <pdeara01@dcs.bbk.ac.uk>
2016-03-21 17:31:58 +0000

    Ups
    Forgot this one in the previous commit.

---
+++ Mastermind/src/controllers/IPegGenerator.java
@@ -0,0 +1,30 @@
+package controllers;
+
+import models.IPeg;
+
+/**
+ * Peg Generator, returning a random peg or a specific
+ * Peg given a colour. If colour specified is not found,
+ * will return null.
+ * 
+ * @author pdeara01
+ *
+ */
+public interface IPegGenerator {
+
+	/**
+	 * Returns a generated Peg from a known random colour list.
+	 * 
+	 * @return IPeg
+	 */
+	public IPeg getAPeg();
+	
+	/**
+	 * For the supplied colour, returns a generated Peg.
+	 * If the supplied colour is not known, it will return null.
+	 * 
+	 * @param colour
+	 * @return IPeg
+	 */
+	public IPeg getPeg(String colour);
+}
