6e612072d2422aad15d246ae82f96bce58753bcf
Pedro Gordo <pedro.gordo1986@gmail.com>
2016-03-27 17:22:36 +0100

    Forgot to add class in previous commit.

---
+++ Mastermind/src/models/PegGenerationError.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package models;
+
+/**
+ * Throwable used in the case that the peg is not generated correctly.
+ * 
+ * @author pedro
+ *
+ */
+public class PegGenerationError extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
