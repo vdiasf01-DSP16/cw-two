cf597d568bc6ccf75ca1ba52c7132d595628c973
Pedro Gordo <pedro.gordo1986@gmail.com>
2016-03-28 12:17:27 +0100

    Minor clean up

--- Mastermind/src/classes/GameImpl.java
+++ Mastermind/src/classes/GameImpl.java
@@ -20,19 +20,23 @@ import views.ITextBeforeGuess;
  * @author Keith Mannock
  *
  */
-public class GameImpl extends GameAbstractImpl {
+public class GameImpl extends GameAbstractImpl
+{
 
 	@Inject
 	private PegGeneratorFactory pegGeneratorFactory;
 	@Inject
 	private StartTextFactory startTextFactory;
+
 	@Inject
-	private GameImpl(@Named("easy") boolean easy) {
+	private GameImpl(@Named("easy") boolean easy)
+	{
 		super(easy);
 	}
 
 	@Override
-	public void runGames() {
+	public void runGames()
+	{
 
 		// Start text
 		IStartText startText = startTextFactory.factoryMethod();
@@ -43,25 +47,33 @@ public class GameImpl extends GameAbstractImpl {
 		IColourLoader colourLoader = colourLoaderFactory.factoryMethod();
 
 		// Create peg generator with loaded colours
-		IPegGenerator pegGenerator = pegGeneratorFactory.create(colourLoader.getColours());
+		IPegGenerator pegGenerator = pegGeneratorFactory
+				.create(colourLoader.getColours());
 
 		CodeGeneratorFactory codeGeneratorFactory = new CodeGeneratorFactory();
-		ICodeGenerator codeGenerator = codeGeneratorFactory.factoryMethod(pegGenerator);
+		ICodeGenerator codeGenerator = codeGeneratorFactory
+				.factoryMethod(pegGenerator);
 		codeGenerator.generateNewCode();
 
 		FlowControllerFactory flowControllerFactory = new FlowControllerFactory();
 		IFlowController flowController = flowControllerFactory.factoryMethod();
 
 		TextBeforeGuessFactory textBeforeGuessFactory = new TextBeforeGuessFactory();
-		ITextBeforeGuess textBeforeGuess = textBeforeGuessFactory.factoryMethod();
+		ITextBeforeGuess textBeforeGuess = textBeforeGuessFactory
+				.factoryMethod();
+
+		/*
+		 * This variable is used to know if the number of tries has finished or
+		 * if the secret code was guessed
+		 */
 		boolean keepGuessing = false;
-		do {
+		do
+		{
 			textBeforeGuess.show(showCode, codeGenerator.getCodeString());
-			
-			
+
 			keepGuessing = flowController.isGameFinished();
-			//TODO change condition for loop
-		} while (false);
+			// TODO change condition for loop
+		} while (keepGuessing);
 	}
 
 }
